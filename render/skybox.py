3be9aa655b973a83f93ea5065959f08a6eeaeada
NachoideV3 <peraltaignacio64@outlook.com.ar>
2024-09-03 18:29:22 -0300

    .

--- render/skybox.py
+++ render/skybox.py
@@ -1,63 +1,69 @@
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
-import numpy as np
 from .loader.texture_loader import TextureLoader
 
 class Skybox:
     def __init__(self):
         self.texture_id = None
-        self.sphere_list = None
         self.texture_loader = TextureLoader()
 
     def initialize(self):
-        # Generar una lista de visualización para la esfera del skybox
-        self.sphere_list = glGenLists(1)
-        glNewList(self.sphere_list, GL_COMPILE)
-        self.create_sphere(radius=50, lats=40, longs=40)
-        glEndList()
+        pass  # No se necesita una lista de visualización para un cubo
 
     def load_texture(self, texture_path):
-        # Usar un nombre genérico para el skybox como 'skybox_texture'
         material_name = 'skybox_texture'
         self.texture_loader.load_texture(material_name, texture_path)
         self.texture_id = self.texture_loader.get_texture(material_name)
-
-
-    def create_sphere(self, radius, lats, longs):
-        """ Crea una esfera para usarla como skybox """
-        for i in range(lats):
-            lat0 = np.pi * (-0.5 + float(i) / lats)
-            z0 = np.sin(lat0)
-            zr0 = np.cos(lat0)
-
-            lat1 = np.pi * (-0.5 + float(i + 1) / lats)
-            z1 = np.sin(lat1)
-            zr1 = np.cos(lat1)
-
-            glBegin(GL_QUAD_STRIP)
-            for j in range(longs + 1):
-                lng = 2 * np.pi * float(j) / longs
-                x = np.cos(lng)
-                y = np.sin(lng)
-
-                glTexCoord2f(float(j) / longs, float(i) / lats)
-                glVertex3f(x * zr0 * radius, y * zr0 * radius, z0 * radius)
-
-                glTexCoord2f(float(j) / longs, float(i + 1) / lats)
-                glVertex3f(x * zr1 * radius, y * zr1 * radius, z1 * radius)
-            glEnd()
 
     def draw(self):
         if self.texture_id:
             glEnable(GL_TEXTURE_2D)
             glBindTexture(GL_TEXTURE_2D, self.texture_id)
 
-        # Renderizar la esfera como skybox
+        # Dibujar un cubo
         glPushMatrix()
         glDisable(GL_LIGHTING)  # Desactivar iluminación para el skybox
         glColor3f(1.0, 1.0, 1.0)  # Color blanco para el skybox
-        glCallList(self.sphere_list)
+
+        # Definir las caras del cubo
+        size = 10  # Tamaño del cubo
+        vertices = [
+            [-size, -size, -size], [size, -size, -size], [size, size, -size], [-size, size, -size],  # Frente
+            [-size, -size, size], [size, -size, size], [size, size, size], [-size, size, size],  # Atrás
+            [-size, -size, -size], [size, -size, -size], [size, -size, size], [-size, -size, size],  # Abajo
+            [-size, size, -size], [size, size, -size], [size, size, size], [-size, size, size],  # Arriba
+            [-size, -size, -size], [-size, size, -size], [-size, size, size], [-size, -size, size],  # Izquierda
+            [size, -size, -size], [size, size, -size], [size, size, size], [size, -size, size]   # Derecha
+        ]
+
+        # Coordenadas de textura para el cubo (ajustar según sea necesario)
+        tex_coords = [
+            [0, 0], [1, 0], [1, 1], [0, 1],  # Frente
+            [0, 0], [1, 0], [1, 1], [0, 1],  # Atrás
+            [0, 0], [1, 0], [1, 1], [0, 1],  # Abajo
+            [0, 0], [1, 0], [1, 1], [0, 1],  # Arriba
+            [0, 0], [1, 0], [1, 1], [0, 1],  # Izquierda
+            [0, 0], [1, 0], [1, 1], [0, 1]   # Derecha
+        ]
+
+        # Dibujar las caras del cubo
+        faces = [
+            [0, 1, 2, 3],  # Frente
+            [4, 5, 6, 7],  # Atrás
+            [8, 9, 10, 11],  # Abajo
+            [12, 13, 14, 15],  # Arriba
+            [16, 17, 18, 19],  # Izquierda
+            [20, 21, 22, 23]  # Derecha
+        ]
+
+        for i, face in enumerate(faces):
+            glBegin(GL_QUADS)
+            for j, vertex_index in enumerate(face):
+                glTexCoord2f(*tex_coords[i * 4 + j])
+                glVertex3f(*vertices[vertex_index])
+            glEnd()
+
         glEnable(GL_LIGHTING)  # Reactivar iluminación después del skybox
         glPopMatrix()
 
