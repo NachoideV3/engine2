020cac121d346d1871ce6df27b1437700bf35f46
NachoideV3 <peraltaignacio64@outlook.com.ar>
2024-09-03 12:33:29 -0300

    skybox

--- render/skybox.py
+++ render/skybox.py
@@ -1,63 +1,65 @@
 from OpenGL.GL import *
+from OpenGL.GLUT import *
 from OpenGL.GLU import *
 import numpy as np
-from PIL import Image
+from .loader.texture_loader import TextureLoader
 
 class Skybox:
     def __init__(self):
-        self.skybox_texture = None
-
-    def load_texture(self, filename):
-        try:
-            # Cargar la imagen y convertirla a formato adecuado para OpenGL
-            image = Image.open(filename)
-            image = image.transpose(Image.FLIP_TOP_BOTTOM)
-            image_data = np.array(image.convert("RGBA"), dtype=np.uint8)
-            internal_format = GL_RGBA
-            format = GL_RGBA
-            type = GL_UNSIGNED_BYTE
-
-            if self.skybox_texture:
-                glDeleteTextures(self.skybox_texture)
-
-            self.skybox_texture = glGenTextures(1)
-            glBindTexture(GL_TEXTURE_2D, self.skybox_texture)
-
-            # Cargar la textura en OpenGL
-            glTexImage2D(GL_TEXTURE_2D, 0, internal_format, image_data.shape[1], image_data.shape[0], 0, format, type, image_data)
-
-            # Establecer parámetros de textura
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
-
-            print(f"Textura del skybox cargada desde {filename}")
-        except Exception as e:
-            print(f"Error al cargar la textura del skybox: {e}")
+        self.texture_id = None
+        self.sphere_list = None
+        self.texture_loader = TextureLoader()
 
     def initialize(self):
-        # Cargar una textura predeterminada aquí, si lo deseas
-        #self.load_texture('path/to/your/default_image.jpg')
-        pass
+        # Generar una lista de visualización para la esfera del skybox
+        self.sphere_list = glGenLists(1)
+        glNewList(self.sphere_list, GL_COMPILE)
+        #self.create_sphere(radius=50, lats=40, longs=40)
+        glEndList()
 
-    def draw(self):
-        if self.skybox_texture:
-            glDisable(GL_DEPTH_TEST)  # Desactiva el test de profundidad para el skybox
-            glEnable(GL_TEXTURE_2D)
-            glBindTexture(GL_TEXTURE_2D, self.skybox_texture)
-            glPushMatrix()
+    def load_texture(self, texture_path):
+        # Usar un nombre genérico para el skybox como 'skybox_texture'
+        material_name = 'skybox_texture'
+        self.texture_loader.load_texture(material_name, texture_path)
+        self.texture_id = self.texture_loader.get_texture(material_name)
 
-            size = 100.0  # Tamaño del skybox, ajusta según sea necesario
 
-            # Dibujar un quad en lugar del cubo para representar el skybox
-            glBegin(GL_QUADS)
-            glTexCoord2f(0, 0); glVertex3f(-size, -size, -size)
-            glTexCoord2f(1, 0); glVertex3f(size, -size, -size)
-            glTexCoord2f(1, 1); glVertex3f(size, size, -size)
-            glTexCoord2f(0, 1); glVertex3f(-size, size, -size)
+    def create_sphere(self, radius, lats, longs):
+        """ Crea una esfera para usarla como skybox """
+        for i in range(lats):
+            lat0 = np.pi * (-0.5 + float(i) / lats)
+            z0 = np.sin(lat0)
+            zr0 = np.cos(lat0)
+
+            lat1 = np.pi * (-0.5 + float(i + 1) / lats)
+            z1 = np.sin(lat1)
+            zr1 = np.cos(lat1)
+
+            glBegin(GL_QUAD_STRIP)
+            for j in range(longs + 1):
+                lng = 2 * np.pi * float(j) / longs
+                x = np.cos(lng)
+                y = np.sin(lng)
+
+                glTexCoord2f(float(j) / longs, float(i) / lats)
+                glVertex3f(x * zr0 * radius, y * zr0 * radius, z0 * radius)
+
+                glTexCoord2f(float(j) / longs, float(i + 1) / lats)
+                glVertex3f(x * zr1 * radius, y * zr1 * radius, z1 * radius)
             glEnd()
 
-            glPopMatrix()
-            glEnable(GL_DEPTH_TEST)  # Reactiva el test de profundidad
+    def draw(self):
+        if self.texture_id:
+            glEnable(GL_TEXTURE_2D)
+            glBindTexture(GL_TEXTURE_2D, self.texture_id)
+
+        # Renderizar la esfera como skybox
+        glPushMatrix()
+        glDisable(GL_LIGHTING)  # Desactivar iluminación para el skybox
+        glColor3f(1.0, 1.0, 1.0)  # Color blanco para el skybox
+        glCallList(self.sphere_list)
+        glEnable(GL_LIGHTING)  # Reactivar iluminación después del skybox
+        glPopMatrix()
+
+        if self.texture_id:
             glDisable(GL_TEXTURE_2D)
