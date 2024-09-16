03836a330a00bf0bf8a985dcc56435e709f6c0eb
NachoideV3 <peraltaignacio64@outlook.com.ar>
2024-09-16 14:06:39 -0300

    .

--- test.py
+++ test.py
@@ -1,18 +1,27 @@
-from PyQt5.QtWidgets import QOpenGLWidget
 from OpenGL.GL import *
+from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
-class SimpleOpenGLWidget(QOpenGLWidget):
-    def initializeGL(self):
-        glEnable(GL_DEPTH_TEST)
-        glClearColor(0.0, 0.0, 0.0, 1.0)
+def check_opengl_info():
+    # Inicializa GLUT para poder usar OpenGL
+    glutInit()
+    
+    # Crea una ventana invisible para obtener la información
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH)
+    glutInitWindowSize(1, 1)
+    glutCreateWindow(b"OpenGL Info")
+    
+    # Obtén información sobre OpenGL
+    gl_version = glGetString(GL_VERSION)
+    gl_renderer = glGetString(GL_RENDERER)
+    gl_vendor = glGetString(GL_VENDOR)
+    gl_shading_language_version = glGetString(GL_SHADING_LANGUAGE_VERSION)
+    
+    # Imprime la información
+    print(f"OpenGL Version: {gl_version.decode('utf-8')}")
+    print(f"Renderer: {gl_renderer.decode('utf-8')}")
+    print(f"Vendor: {gl_vendor.decode('utf-8')}")
+    print(f"Shading Language Version: {gl_shading_language_version.decode('utf-8')}")
 
-    def paintGL(self):
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        glLoadIdentity()
-        glTranslatef(0.0, 0.0, -5)
-        glBegin(GL_TRIANGLES)
-        glVertex3f(-1.0, -1.0, 0.0)
-        glVertex3f(1.0, -1.0, 0.0)
-        glVertex3f(0.0, 1.0, 0.0)
-        glEnd()
+if __name__ == "__main__":
+    check_opengl_info()
